3cc8a929a50a03e61e28db9adbacdd1baf8a236d
Alaninja1209 <alanalfre.2001@gmail.com>
2024-04-13 15:36:04 -0600

    dinamic model

--- src/dynamic_models/scripts/rover_model.py
+++ src/dynamic_models/scripts/rover_model.py
@@ -8,32 +8,35 @@ import math as mt
 class Mobile_robot():
     def __init__(self):
         # Ros node
-        rospy.init_node("rover_model")
+        rospy.init_node("rover_simulation")
 
-        # Mobile robot variables
-        self.x = 0.0
-        self.y = 0.0
-        self.r = 0.0
+        # Torques de cada uno de los motores
+        self.Tr = 0.0
+        self.Tl = 0.0
+        self.r = 0.0 # Radius of the robot
         self.theta = 0.0
-        self.r = 0.0
+        self.w = 0.0
         self.v = 0.0
-        self.a = 0.0
-        self.m = 0.0
 
-        # Fuerzas longitudinales
-        self.Fl = 0.0
-        self.Fr = 0.0
+    def model_rover(self, m): # Pasar masa como argumento
+        # Calculate forces 
+        Fr = self.Tr / self.r
+        Fl = self.Tl / self.r
 
-    def motion_robot(self):
-        # Calculate robot force
-        F = self.Fl + self.Fr
+        # Generate robot forces
+        F = Fr + Fl
 
-        # Generar fuerza del robot y movimiento del robot
-        self.x = self.v * mt.cos(self.theta)
-        self.a = self.v * mt.cos(self.theta) - self.v * mt.sin(self.theta) * self.w
+        # Calculate speed, aceleration and position in x
+        x = self.v * mt.sin(self.theta)
+        vx = self.v * mt.cos(self.theta) 
+        ax = (F / m) * mt.cos(self.theta) - vx * mt.sin(self.theta) * self.w
 
-        # Para la posicion de y
-        self.y = (F / self.m) * mt.sin(self.theta) + self.v * mt.cos(self.theta) * self.w
+        # Calculate speed, aceleration and position for y
+        y = self.v * mt.cos(self.theta)
+        vy = self.v * mt.sin(self.theta)
+        ay = (F / m) * mt.sin(self.theta) + vy * mt.cos(self.theta) * self.w
+
+        # Publish v, a and x
 
 
 if __name__=='__main__':
