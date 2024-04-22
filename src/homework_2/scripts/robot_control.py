6ce755c18c6242ce1f55778347c3671e7c845fcf
Pablo Ortega <2001.paok@gmail.com>
2024-04-21 18:54:17 -0600

    Added theta plot

--- src/homework_2/scripts/robot_control.py
+++ src/homework_2/scripts/robot_control.py
@@ -21,11 +21,14 @@ class Rover():
         self.theta_dot = 0
 
         self.desired_state_dot = np.mat([[0, 0]]).T
-        self.desired_state = np.mat([[2, 2]]).T # location from origin in meters as reference (x,y)
+        self.desired_state = np.mat([[-4, 6]]).T # location from origin in meters as reference (x,y)
 
         self.phi_mat = np.mat([[self.r/self.d, -self.r/self.d]])
 
+        self.angles = []
+
         _, self.sim_plot   = plt.subplots(1, figsize = (5,5))
+        _, self.angle_plot = plt.subplots(1, figsize = (5,5))
             
     def linear_sim(self):
         k_mat = np.mat([[1, 0],
@@ -40,15 +43,17 @@ class Rover():
             self.q_dot = self.desired_state_dot + k_mat*(error)
             D_mat = self.get_angle_matrix()
             self.theta_dot = self.phi_mat*np.linalg.inv(D_mat)*(self.desired_state_dot + k_mat*error)
-            print(self.theta_dot)
             self.q = self.q + self.q_dot *dt
-            self.theta += self.theta + self.theta_dot[0,0] * dt
+            self.theta = self.theta + self.theta_dot[0,0] * dt
+            self.theta = self.theta % 2*np.pi
+            self.angles.append(self.theta)
 
             prev_time = time.time()
             self.visualize_position()
                 
             
     def get_angle_matrix(self):
+        print("Theta ", self.theta)
         return np.mat([[self.r/2*np.cos(self.theta)-self.h*self.r/self.d*np.sin(self.theta), self.r/2*np.cos(self.theta)+self.h*self.r/self.d*np.sin(self.theta)],
                       [self.r/2*np.sin(self.theta)-self.h*self.r/self.d*np.cos(self.theta), self.r/2*np.sin(self.theta)+self.h*self.r/self.d*np.cos(self.theta)]])
         
@@ -56,12 +61,14 @@ class Rover():
     def visualize_position(self):
         
         self.sim_plot.clear()
+        self.angle_plot.clear()
         
         x1 = self.q[0,0]
         y1 = self.q[1,0]
         self.sim_plot.plot([0, x1], [0, y1], lw=2, c='k') 
         self.sim_plot.plot(x1, y1, 'bo', markersize=10, c= '#222E50')
         self.sim_plot.plot(self.desired_state[0,0], self.desired_state[1,0], 'bo')
+        self.angle_plot.plot(self.angles)
         self.sim_plot.set_xlim(-10,10)
         self.sim_plot.set_ylim(-10,10)
 
