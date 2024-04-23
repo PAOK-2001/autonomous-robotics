0a75a932b639d1e71a3958628d5fea74102c4d80
Pablo Ortega <2001.paok@gmail.com>
2024-04-22 18:19:17 -0600

    Added control output plot

--- src/homework_2/scripts/robot_control.py
+++ src/homework_2/scripts/robot_control.py
@@ -26,29 +26,34 @@ class Rover():
         self.phi_mat = np.mat([[self.r/self.d, -self.r/self.d]])
 
         self.angles = []
+        self.pos_x = []
+        self.pos_y = []
+        self.control = []
 
         _, self.sim_plot   = plt.subplots(1, figsize = (5,5))
         _, self.angle_plot = plt.subplots(1, figsize = (5,5))
+        _, self.control_plot = plt.subplots(1, figsize = (5,5))
+
             
     def linear_sim(self):
         k_mat = np.mat([[1, 0],
-                       [0, 1]])
+                       [0, 2]])
 
         prev_time = time.time()
         print("Simulating by Newton-Euler")
-        
         while(True):
-            dt = time.time() - prev_time
-            error = self.desired_state-self.q
-            self.q_dot = self.desired_state_dot + k_mat*(error)
+            dt = 0.001
             D_mat = self.get_angle_matrix()
-            self.theta_dot = self.phi_mat*np.linalg.inv(D_mat)*(self.desired_state_dot + k_mat*error)
+            error = self.desired_state-self.q
+            control_out = np.linalg.pinv(D_mat)*(self.desired_state_dot + k_mat*error)
+            self.control.append(control_out[0,0])
+            self.q_dot = self.desired_state_dot + k_mat*(error)
+            self.theta_dot = self.phi_mat*np.linalg.pinv(D_mat)*(self.desired_state_dot + k_mat*error)
             self.q = self.q + self.q_dot *dt
             self.theta = self.theta + self.theta_dot[0,0] * dt
-            self.theta = self.theta % 2*np.pi
+            # self.theta = self.theta % 2*np.pi
             self.angles.append(self.theta)
-
-            prev_time = time.time()
+            # prev_time = time.time()
             self.visualize_position()
                 
             
@@ -58,19 +63,26 @@ class Rover():
                       [self.r/2*np.sin(self.theta)-self.h*self.r/self.d*np.cos(self.theta), self.r/2*np.sin(self.theta)+self.h*self.r/self.d*np.cos(self.theta)]])
         
 
+
     def visualize_position(self):
         
         self.sim_plot.clear()
         self.angle_plot.clear()
+        self.control_plot.clear()
         
         x1 = self.q[0,0]
         y1 = self.q[1,0]
-        self.sim_plot.plot([0, x1], [0, y1], lw=2, c='k') 
-        self.sim_plot.plot(x1, y1, 'bo', markersize=10, c= '#222E50')
-        self.sim_plot.plot(self.desired_state[0,0], self.desired_state[1,0], 'bo')
+        self.pos_x.append(x1)
+        self.pos_y.append(y1)
+        #self.sim_plot.plot([0, x1], [0, y1], lw=2, c='k') 
+        #self.sim_plot.plot(x1, y1, 'bo', markersize=10, c= '#222E50')
+        self.sim_plot.plot(self.pos_x, c='r')
+        self.sim_plot.plot(self.pos_y, c='b')
+        # self.sim_plot.plot(self.desired_state[0,0], self.desired_state[1,0], 'bo')
         self.angle_plot.plot(self.angles)
-        self.sim_plot.set_xlim(-10,10)
-        self.sim_plot.set_ylim(-10,10)
+        self.control_plot.plot(self.control)
+        # self.sim_plot.set_xlim(-10,10)
+        # self.sim_plot.set_ylim(-10,10)
 
         plt.pause(0.0004)
         
