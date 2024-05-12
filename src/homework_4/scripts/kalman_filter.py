Fixed covariance estimation
Co-authored-by: AlejandroFloresM02 <A01634167@tec.mx>
Co-authored-by: abrahamdaf <A01634133@tec.mx>
Co-authored-by: Jairjim <A01632853@tec.mx>

src/homework_4/scripts/kalman_filter.py
@@ -2,9 +2,11 @@ import numpy as np
 from numpy.linalg import inv 
 import pandas as pd
 import matplotlib.pyplot as plt
+
+
     
 if __name__ == "__main__":
-    np.random.seed(42)
+    # np.random.seed(42)
     # Simulation Details
     sim_step = 0.001 
     sim_len = 7
@@ -21,13 +23,13 @@ if __name__ == "__main__":
     
     t = np.arange(0, sim_len, sim_step)
     A = np.zeros([3,3])
-    P = np.eye(3,3)
 
     # Kalman parameters
     C = np.eye(3,3)
+    Q = np.eye(3,3)
     K =  np.array([  [0.005,0,0], [0,0.005,0], [0,0,0.005] ])
+    P = np.zeros([3,3])
     x_hat = np.array([[0, 0, 0]]).T  # Initial States
-    p_hat = np.zeros([3,3])
     out = np.concatenate((x.T,x_hat.T),axis = 1)
 
     for i in t[:-1]:
@@ -44,19 +46,22 @@ if __name__ == "__main__":
                     phi[0]])
         
         
-        x_hat_dot = M@u - P@C.T@inv(K)@(x_hat - x)
-        p_hat_dot = A@P + P@A.T + P - P@C.T@inv(K)@C@P
+        x_hat_dot = M@u - P@inv(K)@(x_hat - x)
+        p_hat_dot = Q - P@inv(K)@P
 
         x_hat = x_hat + x_hat_dot*sim_step
-        p_hat = p_hat + p_hat_dot*sim_step
-
+        P = P + p_hat_dot*sim_step
+        
+        x = x + sim_step*(M@u + noise) 
         states = np.concatenate((x.T, x_hat.T), axis=1)
         out = np.concatenate((out,states), axis=0)
         
-        x = x + sim_step*(M@u + noise) 
+        
    
     t = t.reshape(-1, 1)
     out = np.concatenate((out, t), axis= 1)
-    out_df = pd.DataFrame(out, columns=['x', 'y', 'theta', 'x_hat', 'y_hat', 'theta_hat', 'time'])
-    out_df.plot(x='time',y=['x',"x_hat"])
+    out_df = pd.DataFrame(out, columns=['Position X', 'Position Y', 'Theta', 'Kalman Filtered X', 'Kalman Filtered Y', 'Kalman Filtered Theta', 'Time'])
+    out_df.plot(x='Time',y=['Position X',"Kalman Filtered X"])
+    plt.xlabel('Time (seconds)', fontsize=18)
+    plt.ylabel('X position (meters)', fontsize=16)
     plt.show()  