bf9aeaa592d63aa070be85f67126c3510c426688
Pablo Ortega <2001.paok@gmail.com>
2024-05-12 17:21:58 -0600

    Eliminated joseph KF version
    Co-authored-by: AlejandroFloresM02 <A01634167@tec.mx>
    Co-authored-by: abrahamdaf <A01634133@tec.mx>
    Co-authored-by: Jairjim <A01632853@tec.mx>

--- src/homework_4/scripts/kalman_filter.py
+++ src/homework_4/scripts/kalman_filter.py
@@ -2,41 +2,8 @@ import numpy as np
 from numpy.linalg import inv 
 import pandas as pd
 import matplotlib.pyplot as plt
-
-
-# class KalmanFilter():
-#     def __init__(self):
-#         pass
-
-#     @staticmethod
-#     def predict(system_coefficients, system_input, 
-#                 prev_states, prev_covariance, process_noise, 
-#                 sim_step):
-        
-#         A, B = system_coefficients
-#         estimated_states = A@prev_states + B@system_input
-#         estimated_covariance = A@prev_covariance@A.T + process_noise
-
-#         return (estimated_states, estimated_covariance)
-        
-#     @staticmethod
-#     def update(measurements, states, covariance, 
-#                obser_mat, obser_noise, kalman_gain,
-#                sim_step):
-#         breakpoint()
-#         error = measurements - obser_mat@states
-#         error_covariance = obser_mat@(covariance@obser_mat.T) + obser_noise
-#         # breakpoint()
-#         updated_gain = (covariance@obser_mat.T)@inv(error_covariance) # must invert
-#         updated_state = states + updated_gain@error
-        
-#         I = np.eye(len(updated_gain))
-#         updated_covariance = (I-updated_gain@obser_mat)@covariance@(I-updated_gain*obser_mat).T + updated_gain@obser_noise@updated_gain.T
-        
-#         return (updated_state *sim_step, updated_covariance*sim_step, updated_gain)
     
 if __name__ == "__main__":
-
     np.random.seed(42)
     # Simulation Details
     sim_step = 0.001 
@@ -77,35 +44,16 @@ if __name__ == "__main__":
                     phi[0]])
         
         
-        # x_hat_dot, p_hat_dot = KalmanFilter.predict(system_coefficients=(A, M),
-        #                                 system_input= u,
-        #                                 prev_states= x,
-        #                                 prev_covariance=P,
-        #                                 process_noise= noise, 
-        #                                 sim_step = sim_step)
-        
-        # Simulate model step
-        # x_hat = x_hat + x_hat_dot
-        # p_hat = p_hat + p_hat_dot
-        
-        x = x + sim_step*(M@u + noise) 
-        # x_hat_dot = M@u - P@C.T@inv(K)@(x_hat - x)
-        # p_hat_dot = A@P + P@A.T + P - P@C.T@inv(K)@C@P
-        # x_hat_dot, p_hat_dot, K = KalmanFilter.update(measurements = x,
-        #                                     states= x_hat,
-        #                                     covariance= p_hat,
-        #                                     obser_mat= C, 
-        #                                     obser_noise=P, 
-        #                                     kalman_gain=K, 
-        #                                     sim_step= sim_step)
+        x_hat_dot = M@u - P@C.T@inv(K)@(x_hat - x)
+        p_hat_dot = A@P + P@A.T + P - P@C.T@inv(K)@C@P
 
         x_hat = x_hat + x_hat_dot*sim_step
         p_hat = p_hat + p_hat_dot*sim_step
-        
-        # breakpoint()
 
         states = np.concatenate((x.T, x_hat.T), axis=1)
         out = np.concatenate((out,states), axis=0)
+        
+        x = x + sim_step*(M@u + noise) 
    
     t = t.reshape(-1, 1)
     out = np.concatenate((out, t), axis= 1)
